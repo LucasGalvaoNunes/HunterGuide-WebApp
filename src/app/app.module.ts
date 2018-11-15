24c869b36011a9d309b3cba3a0bead5661c4571b
Lucas Galvão Nunes <lucas_galvaonunes@hotmail.com>
2018-11-15 16:44:32 -0200

    Criado as telas principais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {ProfilePage} from "../pages/profile/profile";
+import {GuidePage} from "../pages/guide/guide";
+import {FavoritesPage} from "../pages/favorites/favorites";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    ProfilePage,
+    GuidePage,
+    FavoritesPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    ProfilePage,
+    GuidePage,
+    FavoritesPage,
     TabsPage
   ],
   providers: [
